392388531962bfba8b2f11346aff2dc57f103dd2
Hamdy <tantaacademyt196@gmail.com>
2025-03-23 21:51:27 +0000

    Add: Header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,7 +1,21 @@
 function Header() {
   return (
     <>
-
+      <header className="bg-white shadow py-6">
+        <div className="container flex justify-between items-center mx-auto px-2">
+          <div className="flex items-center">
+            <svg className="w-8 h-8 text-indigo-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+              />
+            </svg>
+            <h1 className="text-2xl font-bold text-gray-900">TaskFlow</h1>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
