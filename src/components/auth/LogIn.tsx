3a6b8b2ee7c5ae54e03cca54adceca702e1132a5
Hamdy <tantaacademyt196@gmail.com>
2025-04-04 17:11:15 +0100

    Update: Login Component DarkMode

--- src/components/auth/LogIn.tsx
+++ src/components/auth/LogIn.tsx
@@ -35,11 +35,11 @@ function LogIn({users } : UsersProps) {
   return (
     <section>
       <div className="container min-h-[85vh] mx-auto flex items-center justify-center font-[poppins]">
-        <form onSubmit={(e) => e.preventDefault()} method="post" className="grid gap-4 max-w-[34rem] w-full p-6 rounded-md border shadow-sm  bg-white">
-          <h2 className="text-center text-2xl font-bold mb-2 text-gray-900">Login</h2>
+        <form onSubmit={(e) => e.preventDefault()} method="post" className="grid gap-4 max-w-[34rem] w-full p-6 rounded-md border shadow-sm dark:border-[#444444] dark:bg-[#121212]  bg-white">
+          <h2 className="text-center text-2xl font-bold mb-2 text-gray-900 dark:text-[#E0E0E0]">Login</h2>
 
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="block text-base font-medium text-gray-900">
+            <label htmlFor="email" className="block text-base font-medium text-gray-900 dark:text-[#E0E0E0]">
               Email
             </label>
             <input
@@ -50,12 +50,12 @@ function LogIn({users } : UsersProps) {
                 setFormInputs({ ...formInputs, email: e.target.value });
               }}
               placeholder="Enter your email"
-              className="block w-full p-2 text-base font-medium shadow-xs text-gray-900 bg-transparent border border-gray-300 rounded-lg placeholder-gray-400 focus:outline-none leading-relaxed "
+              className="block w-full p-2 text-base font-medium shadow-xs text-gray-900 dark:text-[#E0E0E0] bg-transparent border border-gray-300 rounded-lg placeholder-gray-400 focus:outline-none leading-relaxed "
             />
           </div>
 
           <div className="flex flex-col gap-2 relative">
-            <label htmlFor="password" className="block text-base font-medium text-gray-900">
+            <label htmlFor="password" className="block text-base font-medium text-gray-900 dark:text-[#E0E0E0]">
               Password
             </label>
             <input
@@ -66,21 +66,21 @@ function LogIn({users } : UsersProps) {
                 setFormInputs({ ...formInputs, password: e.target.value });
               }}
               placeholder="Enter your password"
-              className="block w-full p-2 text-base font-medium shadow-xs text-gray-900 bg-transparent border border-gray-300 rounded-lg placeholder-gray-400 focus:outline-none leading-relaxed "
+              className="block w-full p-2 text-base font-medium shadow-xs text-gray-900 dark:text-[#E0E0E0] bg-transparent border border-gray-300 rounded-lg placeholder-gray-400 focus:outline-none leading-relaxed "
             />
-            <button onClick={()=>setShowPassword(prev => !prev)} className="absolute right-3 bottom-2 cursor-pointer text-gray-800">{showPassword ?<i className="fa-solid fa-eye"></i> : <i className="fa-solid fa-eye-slash"></i>}</button>
+            <button onClick={()=>setShowPassword(prev => !prev)} className="absolute right-3 bottom-[.62rem] cursor-pointer text-gray-800 dark:text-[#B0B0B0]">{showPassword ?<i className="fa-solid fa-eye"></i> : <i className="fa-solid fa-eye-slash"></i>}</button>
             </div>
 
           <input
             type="submit"
             onClick={LogIn}
             value="LogIn"
-            className="w-full mt-2 font-medium text-base bg-indigo-600 text-white py-2 px-4 rounded-md cursor-pointer hover:bg-indigo-700 transition duration-300 "
+            className="w-full mt-2 font-medium text-base bg-indigo-600 dark:bg-[#444444] dark:hover:bg-[#888888] text-white py-2 px-4 rounded-md cursor-pointer hover:bg-indigo-700 transition duration-300 "
           />
 
           <p className="mt-1 text-center text-base">
-            <span className="font-normal">Don’t have an Account ?</span>
-            <Link to="/signup" className="ml-2 font-medium underline hover:text-indigo-700">
+            <span className="font-normal dark:text-[#E0E0E0]">Don’t have an Account ?</span>
+            <Link to="/signup" className="ml-2 font-medium underline hover:text-indigo-700 dark:text-[#E0E0E0] dark:hover:text-[#888888]">
               SignUp
             </Link>
           </p>
